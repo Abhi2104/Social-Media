1ae2a50a3a44f6b9a2d01175708c1620e169d073
Abhijeet Koshti <abhijeetkoshti71@gmail.com>
2024-07-17 02:56:04 +0530

    forms added

---
+++ Social-Media/src/app/signup/signup.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent {
+
+  
+    
+  
+  
+
+
+
+}
