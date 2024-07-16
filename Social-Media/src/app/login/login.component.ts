b4a53bd664dfd12f859b1a4cc06169f38212ed43
Abhijeet <abhijeetkoshti71@gmail.com>
2024-07-17 04:19:55 +0530

    forms added

--- Social-Media/src/app/login/login.component.ts
+++ Social-Media/src/app/login/login.component.ts
@@ -1,12 +1,44 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule,RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  loginError: string |null=null;
+  
+
+  constructor(private fb: FormBuilder, private http: HttpClient,private router: Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required,Validators.minLength(6)]]
+    });
+  }
+
+  onLogin(): void {
+    if (this.loginForm.valid) {
+      const formData = this.loginForm.value;
+      this.http.post('http://localhost:8080/user/login', formData, { responseType: 'text' })
+        .subscribe({
+          next: (response: any) => {
+            console.log(response);
+            this.router.navigate(['/home']);
+          },
+          error: (error) => {
+            console.error('Login error:', error);
+            this.loginError = 'Failed to login. Please try again.';
+          }
+        });
+    }
+  }
+
+  
 
 }
--- Social-Media/src/app/signup/signup.component.ts
+++ Social-Media/src/app/signup/signup.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder,FormGroup,Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, NgModule } from '@angular/core';
+import { FormBuilder,FormGroup,ReactiveFormsModule,Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule,RouterModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
 
-  
+  signupForm: FormGroup;
+
+  constructor(private http: HttpClient, private fb: FormBuilder,private router: Router) {
+    this.signupForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  registerUser() {
+    if (this.signupForm.valid) {
+      this.http
+        .post('http://localhost:8080/user/save', this.signupForm.value)
+        .subscribe({
+          next: (res: any) => {
+            console.log(res);
+            alert('User Created');
+            this.signupForm.reset();
+            
+          },
+          error: (error: any) => {
+            console.error('Error:', error);
+            alert('Registration failed. Please try again.');
+          },
+        });
+    } else {
+      alert('Please fill out the form correctly.');
+    }
+  }
     
   
   
